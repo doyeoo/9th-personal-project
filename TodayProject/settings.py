2d5c9491aabed7f0a270213fd3b530ae800361fe
doyeoo <ehdusla013@gmail.com>
2021-10-06 01:25:49 +0900

    위치 수정

--- TodayProject/settings.py
+++ TodayProject/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'opp2-u3zdd*+3*c9h6o9vh)nibalw7ewl&jo-&2p2_1v33afn0'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','opp2-u3zdd*+3*c9h6o9vh)nibalw7ewl&jo-&2p2_1v33afn0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG',True))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +124,8 @@ MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'account.CustomUser'
+
+#Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
